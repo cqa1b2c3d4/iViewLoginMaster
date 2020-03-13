a452c534cf9c8761aa639d9bd44d21bce0820b21
cqa1b2c3d4 <kgg8899@163.com>
2020-03-13 18:47:19 +0800

    1.去除直播水印； 2.新增5个直播间预览入口。

--- src/view/page-home/components/HelloWorld.vue
+++ src/view/page-home/components/HelloWorld.vue
@@ -1,87 +1,121 @@
 <template>
   <div class="hello">
-    <div>
+    <div class="live-show-container">
       <div id="video"></div>
-      <div>
+      <div class="live-list">
         <ul>
-          <li v-for=" liveItem in liveList"></li>
+          <li :class="{'active': index === clickIndex}" v-for="(item,index) in liveList" :key="item.id" @click="clickThisLive(index)"><div ><img style="height: 100%; margin: 0 auto" :src="item.avatar"></div></li>
         </ul>
       </div>
     </div>
     <h2>测试链接</h2>
     <ul>
       <li>
-        <a @click="resetTabName">
-          socket test page
-        </a>
+        <a @click="resetTabName">socket test page</a>
       </li>
       <li>
-        <a
-          href="javascript:void(0)" @click="tokenTest"
-        >
-          token接口测试
-        </a>
+        <a href="javascript:void(0)" @click="tokenTest">token接口测试</a>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-    export default {
-        name: 'helloWorld',
-        data() {
-            return {
-                videoObject: {
-                    container: "#video", //“#”代表容器的ID，“.”或“”代表容器的class
-                    variable: "player", //该属性必需设置，值等于下面的new chplayer()的对象
-                    autoplay: true, //自动播放
-                    live: false,
-                    loop: true,
-                    debug: true, //是否开启调试模式
-                    drag: 'start', //拖动的属性
-                    seek: 0, //默认跳转的时间
-                    video: "" //视频地址(必填)
-                },
-            }
+  export default {
+    name: 'helloWorld',
+    data() {
+      return {
+        clickIndex: 0,
+        testLiveUrl: 'https://qiniu.00yuyin.com/233333.mp4',
+        videoObject: {
+          container: "#video", //“#”代表容器的ID，“.”或“”代表容器的class
+          variable: "player", //该属性必需设置，值等于下面的new chplayer()的对象
+          autoplay: true, //自动播放
+          live: false,
+          loop: true,
+          debug: true, //是否开启调试模式
+          drag: 'start', //拖动的属性
+          seek: 0, //默认跳转的时间
+          video: "" //视频地址(必填)
         },
-        computed: {
-            userName() {
-                let token = sessionStorage.getItem('TOKEN');
-                if (token === null || token === '') {
-                    return "登录";
-                } else {
-                    this.login = 'javascript:void(0)';
-                    console.log(this.$store.state);
-                    return this.$store.state.nickName;
-                }
-            }
-        },
-        mounted() {
-            this.videoObject.video = 'https://qiniu.00yuyin.com/233333.mp4';
-            let player = new ckplayer(this.videoObject);
-        },
-        methods: {
-            tokenTest() {
-                console.log('start token test');
-                let token = sessionStorage.getItem('TOKEN');
-                if (token !== null && token !== '') {
-                    console.log('token exist, start connect to api');
-                    this.$api.api_login.token_test().then((response) => {
-                        let _data = response.data;
-                        console.log(_data);
-                    }).catch((error) => {
-                        console.log(error)
-                    })
-
-                }
-
-            },
-            resetTabName() {
-                this.$store.commit('changeActiveTab', '4')
-                this.$router.push('/socketTest')
-            }
+        liveList: [
+          {
+            id: '34029',
+            nickName: '夏诗沫直播回放',
+            url: 'https://qiniu.00yuyin.com/233333.mp4',
+            avatar: 'https://qiniu.9kyouxi.com/20190604/5cf609bdf0d4c.jpg'
+          },
+          {
+            id: '738115',
+            nickName: '顺顺宝贝',
+            url: 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4', //张靓颖《我的梦》
+            avatar: 'http://qiniu.9kyouxi.com/20191209175211_92fbe3c38ef9387116ebef81413013d1?imageView2/2/w/600/h/600'
+          },
+          {
+            id: '738045',
+            nickName: '是娜娜呀～',
+            url: 'http://img.ksbbs.com/asset/Mon_1703/d0897b4e9ddd9a5.mp4',
+            avatar: 'http://qiniu.9kyouxi.com/20200112144018_aa4855d1ce56cc4a0d5f52645912cf5b?imageView2/2/w/600/h/600'
+          },
+          {
+            id: '340292',
+            nickName: '夏诗沫直播回放',
+            url: 'https://qiniu.00yuyin.com/233333.mp4',
+            avatar: 'https://qiniu.9kyouxi.com/20190604/5cf609bdf0d4c.jpg'
+          },
+          {
+            id: '340293',
+            nickName: '夏诗沫直播回放',
+            url: 'https://qiniu.00yuyin.com/233333.mp4',
+            avatar: 'https://qiniu.9kyouxi.com/20190604/5cf609bdf0d4c.jpg'
+          }
+        ],
+      }
+    },
+    computed: {
+      userName() {
+        let token = sessionStorage.getItem('TOKEN');
+        if (token === null || token === '') {
+          return "登录";
+        } else {
+          this.login = 'javascript:void(0)';
+          console.log(this.$store.state);
+          return this.$store.state.nickName;
         }
+      }
+    },
+    mounted() {
+      this.videoObject.video = this.testLiveUrl;
+      let player = new ckplayer(this.videoObject);
+    },
+    methods: {
+      clickThisLive(index){
+        this.clickIndex = index;
+        this.videoObject.video = this.liveList[index].url;
+        let player = new ckplayer(this.videoObject);
+
+      },
+      tokenTest() {
+        console.log('start token test');
+        let token = sessionStorage.getItem('TOKEN');
+        if (token !== null && token !== '') {
+          console.log('token exist, start connect to api');
+          this.$api.api_login.token_test().then((response) => {
+            let _data = response.data;
+            console.log(_data);
+          }).catch((error) => {
+            console.log(error)
+          })
+
+        }
+
+      },
+      resetTabName() {
+        this.$store.commit('changeActiveTab', '4')
+        this.$router.push('/socketTest')
+      }
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -90,8 +124,9 @@
   .hello {
     background: url(/static/images/home/index_live_bg.png) no-repeat;
     background-size: 100% 500px;
-    max-width: 1200px;
-    min-width: 800px;
+    width: 80%;
+    max-width: 1800px;
+    min-width: 1200px;
     margin: 0 auto;
   }
 
@@ -114,6 +149,39 @@
   }
 
   #video {
+    width: 80%;
+    height: 100%;
+    float: left;
+  }
+  .live-list{
+    width: 20%;
+    float: left;
+  }
+  .live-list li{
+    padding: 10px 0 10px 0;
+    margin: 0;
+    width: 100%;
+    height: 100px;
+    display: block;
+    float: left;
+  }
+  .live-list li div{
+    background: #c3c3c3;
+    opacity: 0.5;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  .live-list li.active{
+    border: 2px #d19a66 solid;
+  }
+
+  .live-list li div:hover {
+    background: #d19a66;
+    opacity: 1;
+  }
+  .live-show-container{
     margin: 0 auto;
     width: 70%;
     height: 500px;
